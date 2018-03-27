91d438f4fe7fffa9a08131665df84450c1a6a023
emiliesedziol <sedziole@gmail.com>
2018-03-27 12:44:20 -0400

    working with json.server

---
+++ com.org.api/src/test/java/APITesting/com/org/classes/Post2.java
@@ -0,0 +1,39 @@
+package APITesting.com.org.classes;
+
+
+public class Post2 {
+
+	
+	private String id;
+	private String title;
+	private String author;
+	private Info2 info2;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Info2 getInfo2() {
+		return info2;
+	}
+	public void setInfo2(Info2 info2) {
+		this.info2 = info2;
+	}
+	
+	
+	
+}
---
+++ com.org.api/src/test/java/APITesting/com/org/classes/advanced/Post3.java
@@ -0,0 +1,36 @@
+package APITesting.com.org.classes.advanced;
+
+public class Post3 {
+
+	private String id;
+	private String title;
+	private String author;
+	private Info3[] info3;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Info3[] getInfo3() {
+		return info3;
+	}
+	public void setInfo3(Info3[] info3) {
+		this.info3 = info3;
+	}
+
+	
+}
